a119e84e10c9f9ff6f873312128b2574ee24f852
Amir Hesam <hosseinharatii@gmail.com>
2023-05-24 17:21:24 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 #enter answer here
+
+n = str(input(""))
+three = n.split()
+one = int(three[0])
+two = int(three[1])
+tree = int(three[2])
+if one > 0 and two > 0 and tree > 0 and (one+two+tree)==180:
+    print("Yes")
+else:
+    print("No")
